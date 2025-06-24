15c37cefb9b17391e4d1857ecf3f88bea913770c
JohnStewartEAP <124321883+JohnStewartEAP@users.noreply.github.com>
2025-06-24 16:07:20 +1200

    cp

---
+++ Common/GlobalConstants.cs
@@ -0,0 +1,17 @@
+﻿
+/// <summary>
+/// Common constants between all projects
+/// Created: June 2025
+/// [*Licence*]
+/// Author: John Stewart
+/// </summary>
+
+namespace Common
+{
+    public class GlobalConstants
+    {
+        public const int LenCode        = 15;
+        public const int LenDescription = 50;
+        public const int LenLangCode    = 4;
+    }
+}
