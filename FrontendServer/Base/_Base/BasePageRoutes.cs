562757990c5f7e90024974d50323c9bd009fe5f0
JohnStewartEAP <124321883+JohnStewartEAP@users.noreply.github.com>
2025-07-02 12:25:02 +1200

    cp

--- FrontendServer/Base/_Base/BasePageRoutes.cs
+++ FrontendServer/Base/_Base/BasePageRoutes.cs
@@ -3,13 +3,16 @@
     public class BasePageRoutes
     {
         //Routes
+        public const string LogoutRoute    = "logout";
+        public const string ChangePWRoute  = "changePW";
         public const string OrgRoute       = "organisation";
         public const string ConfigRoute    = "config";
         public const string PermRoute      = "permission";
         public const string RolesRoute     = "role";
         public const string AuditRoute     = "audit";
-        
+
         //Page Codes
+        public const string ChangePWCode   = "cpw001";
         public const string OrgPageCode    = "org001";
         public const string ConfigPageCode = "con001";
         public const string PermPageCode   = "per001";
@@ -17,6 +20,7 @@
         public const string AuditPageCode  = "aud001";
 
         static protected readonly string[] BasePageCodeRoutes = {
+            ChangePWCode,           ChangePWRoute,
             OrgPageCode,            OrgRoute,
             ConfigPageCode,         ConfigRoute,
             PermPageCode,           PermRoute,
